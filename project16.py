fc190f81eb6faec8dcfdc7cb9855a056b5c3915f
Gayanevardanyan1122 <103982690+Gayanevardanyan1122@users.noreply.github.com>
2022-06-23 20:38:53 +0400

    Create project16.py

---
+++ project16.py
@@ -0,0 +1,7 @@
+data=[8, 5, 23, 10, 20]
+def triple(data):
+     *** this function returns a list of triple data members ***
+     res = []
+     for num in data:
+        res.append(num*3)
+     return res
